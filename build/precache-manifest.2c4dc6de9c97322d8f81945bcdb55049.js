9ca4cc251441dcb857afd489ccfb931333ccce58
Ashley D <webdevmvp@gmail.com>
2021-10-31 03:20:34 +0000

    adds files

---
+++ build/precache-manifest.2c4dc6de9c97322d8f81945bcdb55049.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14560f72f11ddaf65153dc05554056fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "084836bcbee6ffe93b3b",
+    "url": "/static/css/main.40c530a1.chunk.css"
+  },
+  {
+    "revision": "2915573f32046ddbc5e9",
+    "url": "/static/js/2.db3f839c.chunk.js"
+  },
+  {
+    "revision": "dce905c520e5581a55966da8c6a9a8ff",
+    "url": "/static/js/2.db3f839c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "084836bcbee6ffe93b3b",
+    "url": "/static/js/main.b58a9e7b.chunk.js"
+  },
+  {
+    "revision": "a22a40cff2f4b46ae433",
+    "url": "/static/js/runtime-main.f25b37d5.js"
+  },
+  {
+    "revision": "ec9349f97b6d82820da219e1f76092bb",
+    "url": "/static/media/ai.ec9349f9.wav"
+  },
+  {
+    "revision": "25c6bdf994c767fa9620eab58b8365be",
+    "url": "/static/media/ai_win.25c6bdf9.wav"
+  },
+  {
+    "revision": "e85993981e811368d4c1a0417d78f790",
+    "url": "/static/media/draw.e8599398.wav"
+  },
+  {
+    "revision": "2d56637cf2e39de64094b7c94114f40e",
+    "url": "/static/media/not_allowed.2d56637c.wav"
+  },
+  {
+    "revision": "e26ffcc23b63c92a9280e5e25606bb1e",
+    "url": "/static/media/player.e26ffcc2.wav"
+  }
+]);
